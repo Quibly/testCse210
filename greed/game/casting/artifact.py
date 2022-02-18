d247053d0c6ea63a39e0c057034d4b369e417548
Quibly <trippy524@yahoo.com>
2022-02-17 17:05:07 -0700

    added test code

---
+++ greed/game/casting/artifact.py
@@ -0,0 +1,55 @@
+from game.casting.actor import Actor
+import datetime;
+
+class Artifact(Actor):
+    """
+    An item of cultural or historical interest. 
+    
+    The responsibility of an Artifact is to provide a message about itself.
+
+    Attributes:
+        _message (string): A short description about the artifact.
+        _last_mod (float): A variable to hold a timestamp.
+    """
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+        self._last_mod = float(0)
+        
+    def get_message(self):
+        """Gets the artifact's message.
+        
+        Returns:
+            string: The message.
+        """
+        return self._message
+    
+    def set_message(self, message):
+        """Updates the message to the given one.
+        
+        Args:
+            message (string): The given message.
+        """
+        self._message = message
+
+    def get_last_mod(self):
+        """Gets the artifact's last modifacation time stamp.
+        
+        Returns:
+            float: The last modifacation time stamp.
+        """
+        return self._last_mod
+
+    def set_last_mod(self):
+        """Updates the last modifaction attribute.
+        
+        Args:
+            last_mod (float): The given timestamp
+        """
+        current_time = datetime.datetime.now()
+        timestamp =current_time.timestamp()
+        self._last_mod = timestamp
+ 
+        
+
+        
---
+++ greed/game/directing/director.py
@@ -0,0 +1,97 @@
+import datetime
+import random
+from game.shared.point import Point
+
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        _keyboard_service (KeyboardService): For getting directional input.
+        _video_service (VideoService): For providing video output.
+    """
+
+    def __init__(self, keyboard_service, video_service):
+        """Constructs a new Director using the specified keyboard and video services.
+        
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+            video_service (VideoService): An instance of VideoService.
+        """
+        self._keyboard_service = keyboard_service
+        self._video_service = video_service
+        self._current_score = int(0)
+        
+    def start_game(self, cast):
+        """Starts the game using the given cast. Runs the main game loop.
+
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        self._video_service.open_window()
+        while self._video_service.is_window_open():
+            self._get_inputs(cast)
+            self._do_updates(cast)
+            self._do_outputs(cast)
+        self._video_service.close_window()
+
+    def _get_inputs(self, cast):
+        """Gets directional input from the keyboard and applies it to the robot.
+        
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        robot = cast.get_first_actor("robots")
+        velocity = self._keyboard_service.get_direction()
+        robot.set_velocity(velocity)        
+
+    def _do_updates(self, cast):
+        """Updates the robot's position and resolves any collisions with artifacts.
+        
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        COLS = 60
+        ROWS = 40
+        CELL_SIZE = 15
+
+        x = random.randint(1, COLS - 1)
+        y = random.randint(1, ROWS - 1)
+        aPosition = Point(x, 1)
+        aPosition = aPosition.scale(CELL_SIZE)
+
+        banner = cast.get_first_actor("banners")
+        robot = cast.get_first_actor("robots")
+        artifacts = cast.get_actors("artifacts")
+        currentTime = datetime.datetime.now()
+        currentTimeStamp = currentTime.timestamp()
+
+        banner.set_text(f"Score: {self._current_score}")
+        max_x = self._video_service.get_width()
+        max_y = self._video_service.get_height()
+        robot.move_next(max_x,max_y)
+                
+        for artifact in artifacts:
+            if robot.get_position().equals(artifact.get_position()):
+                message = artifact.get_message()
+                self._current_score += message
+                banner.set_text(f"Score: {self._current_score}")
+                artifact.set_position(aPosition)
+            if (currentTimeStamp - artifact.get_last_mod()) > .1:
+                #if artifact.get_y().equals(ROWS - 1):
+                aDirection = Point(0, CELL_SIZE)
+                artifact.set_velocity(aDirection)
+                artifact.move_next(max_x, max_y)
+                artifact.set_last_mod()
+        
+    def _do_outputs(self, cast):
+        """Draws the actors on the screen.
+        
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        self._video_service.clear_buffer()
+        actors = cast.get_all_actors()
+        self._video_service.draw_actors(actors)
+        self._video_service.flush_buffer()
